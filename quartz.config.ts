d24bb9649ac6bd4dd8dff34a937c54a21ad7994e
eliana <abstractionintheabyss@gmail.com>
2026-08-13 15:03:42 -0400

    Fix formatting and add newline at end of file

--- quartz.config.ts
+++ quartz.config.ts
@@ -1,5 +1,6 @@
 import { QuartzConfig } from "./quartz/cfg"
 import * as Plugin from "./quartz/plugins"
+
 /**
  * Quartz 4 Configuration
  *
@@ -13,8 +14,8 @@ const config: QuartzConfig = {
     enablePopovers: true,
     analytics: {
       provider: 'umami',
-        host: 'https://cloud.umami.is',
-        websiteId: '163553f5-45c0-4291-8423-20bdf8cd73d0'
+      host: 'https://cloud.umami.is',
+      websiteId: '163553f5-45c0-4291-8423-20bdf8cd73d0'
     },
     locale: "en-US",
     baseUrl: "jubilancy.github.io/kit",
@@ -75,21 +76,25 @@ const config: QuartzConfig = {
       Plugin.Latex({ renderEngine: "katex" }),
     ],
     filters: [Plugin.RemoveDrafts()],
-  emitters: [
-    Plugin.AliasRedirects(),
-    Plugin.ComponentResources(),
-    Plugin.ContentPage(),
-    Plugin.FolderPage(),
-    Plugin.TagPage(),
-    Plugin.ContentIndex({
-      enableSiteMap: true,
-      enableRSS: true,
-    }),
-    Plugin.Assets(),
-    Plugin.Static(),
-    Plugin.Favicon(),
-    Plugin.NotFoundPage(),
-],
+    emitters: [
+      Plugin.AliasRedirects(),
+      Plugin.ComponentResources(),
+      Plugin.ContentPage(),
+      Plugin.FolderPage(),
+      Plugin.TagPage(),
+      Plugin.ContentIndex({
+        enableSiteMap: true,
+        enableRSS: true,
+      }),
+      Plugin.Assets(),
+      Plugin.Static(),
+      Plugin.Favicon(),
+      Plugin.NotFoundPage(),
+    ],
+  },
+  layout: {
+    footer: "Footer",
   },
 }
+
 export default config
